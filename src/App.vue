9e7deeb48e53153b325e05b0298cb67ec51e73b1
Pepijn van den Broek <pepijn@vdbroek.dev>
2022-06-29 05:14:32 +0200

    router-view doesn't work with static site

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
     <v-app>
         <v-main>
-            <router-view />
+            <!-- <router-view /> -->
+            <Home />
         </v-main>
     </v-app>
 </template>
+
+<script setup lang="ts">
+import Home from "~/views/Home.vue";
+</script>
