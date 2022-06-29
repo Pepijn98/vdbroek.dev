c1e7229732f1e6a53731d353cb5cd4a6febaaaa6
Pepijn van den Broek <pepijn@vdbroek.dev>
2022-06-29 14:19:30 +0200

    Remove base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
-    base: process.env.NODE_ENV === "development" ? "/beta/" : "/",
     plugins: [
         vue(),
         vuetify()
