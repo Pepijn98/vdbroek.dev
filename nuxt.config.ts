ad9efa31335e1138db9d800b6cd9d4fa8b5b2c87
Pepijn van den Broek <pepijn@vdbroek.dev>
2022-07-09 23:57:00 +0200

    Add google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     target: "static",
+    modules: [
+        "@nuxtjs/google-fonts"
+    ],
     build: {
         postcss: {
             postcssOptions: {
